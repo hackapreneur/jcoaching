03ce91a1a13e3c877f15c687b1403d863a5b6ff2
Justin Wu <justin@roxollo.com>
2013-12-20 23:53:02 -0800

    fixed smartvid

--- test/controllers/lp_controller_test.rb
+++ test/controllers/lp_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class LandingpageControllerTest < ActionController::TestCase
+class LpControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
